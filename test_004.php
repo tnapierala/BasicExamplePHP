eab079dd7a88494deea4573fd1c55795a01885a0
tnapierala <tnapierala98@gmail.com>
2022-03-17 22:44:02 +0100

    NextLessons

---
+++ test_004.php
@@ -0,0 +1,32 @@
+<?php $t = "Strings"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> <?php echo $t ?> </title>
+</head>
+
+<body>
+
+<h1> Lesson4 </h1> <br><br>
+
+<?php
+    echo "Hello World <br />";
+    echo 'Hello World <br />';
+
+    $greeting = "Hello";
+    $target = "World";
+    $phrase = $greeting . " " . $target;
+    echo $phrase;
+
+    echo "<br /><br />";
+    echo "$phrase Again<br />";
+    echo '$phrase Again<br />';
+    echo "{$phrase} Again<br />";
+
+
+?>
+
+</body>
+</html>
