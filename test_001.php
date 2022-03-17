68ffbea93365358edba9c8da706489d17204de4e
Frazoks <tomek98n@gmail.com>
2022-03-17 17:46:33 +0100

    TwoLessons

---
+++ test_001.php
@@ -0,0 +1,31 @@
+<?php
+$t = "Hello World";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> <?php echo $t ?> </title>
+</head>
+
+<body>
+
+<h1> Lesson1 </h1> <br><br>
+
+<?php
+// single-line comments are like this
+# or like this (less common)
+/*
+ double-line comments are written like this, so that you can keep typing and typing
+ */
+
+//$world = "Hello world";
+//echo strtoupper($world);
+
+echo "Hello World! <br />";
+echo "Hello" . "World! <br />";
+echo 2 + 3;
+?>
+</body>
+</html>
---
+++ test_002.php
@@ -0,0 +1,30 @@
+<?php
+$t = "Variables";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> <?php echo $t ?> </title>
+</head>
+
+<body>
+
+<h1> Lesson2 </h1> <br><br>
+
+<?php
+    $var1 = 10;
+    echo $var1;
+    echo "<br />";
+
+    $var1 = 100;
+    echo $var1;
+    echo "<br />";
+
+    $var2 = "Hello World";
+    echo $var2;
+    echo "<br />";
+?>
+</body>
+</html>
