185b2bccdffad1fe450e4c5c7912a099e56ddf6e
tnapierala <tnapierala98@gmail.com>
2022-03-17 18:07:05 +0100

    CleanCode

--- test_002.php
+++ test_002.php
@@ -4,27 +4,26 @@ $t = "Variables";
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title> <?php echo $t ?> </title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <title> <?php echo $t ?> </title>
+    </head>
+    <body>
 
-<body>
+        <h1> Lesson2 </h1> <br><br>
 
-<h1> Lesson2 </h1> <br><br>
+        <?php
+            $var1 = 10;
+            echo $var1;
+            echo "<br />";
 
-<?php
-    $var1 = 10;
-    echo $var1;
-    echo "<br />";
+            $var1 = 100;
+            echo $var1;
+            echo "<br />";
 
-    $var1 = 100;
-    echo $var1;
-    echo "<br />";
-
-    $var2 = "Hello World";
-    echo $var2;
-    echo "<br />";
-?>
-</body>
+            $var2 = "Hello World";
+            echo $var2;
+            echo "<br />";
+        ?>
+    </body>
 </html>
--- test_003.php
+++ test_003.php
@@ -2,41 +2,40 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title> <?php echo $t ?> </title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <title> <?php echo $t ?> </title>
+    </head>
+    <body>
 
-<body>
+        <h1> Lesson3 </h1> <br><br>
 
-<h1> Lesson3 </h1> <br><br>
+        <?php
+            echo "Type Juggling <br /><br />";
+            $count = "2 cats";
+            echo "Type: " . gettype($count) . "<br />";
 
-<?php
-    echo "Type Juggling <br /><br />";
-    $count = "2 cats";
-    echo "Type: " . gettype($count) . "<br />";
+            $count += 3;
+            echo "Type: " . gettype($count) . "<br /><br />";
 
-    $count += 3;
-    echo "Type: " . gettype($count) . "<br /><br />";
+            $cats = "I have " . $count . " cats. ";
+            echo "Cats: " . gettype($cats) . "<br /><br />";
 
-    $cats = "I have " . $count . " cats. ";
-    echo "Cats: " . gettype($cats) . "<br /><br />";
+            echo "Type Casting <br /><br />";
+            settype($count, "integer");
+            echo "count: " . gettype($count) . "<br /><br />";
 
-    echo "Type Casting <br /><br />";
-    settype($count, "integer");
-    echo "count: " . gettype($count) . "<br /><br />";
+            $count2 = (string) $count;
+            echo "count: " . gettype($count) . "<br />";
+            echo "count2: " . gettype($count2) . "<br /><br />";
 
-    $count2 = (string) $count;
-    echo "count: " . gettype($count) . "<br />";
-    echo "count2: " . gettype($count2) . "<br /><br />";
+            $test1 = 3;
+            $test2 = 3;
 
-    $test1 = 3;
-    $test2 = 3;
-
-    settype($test1, "string");
-    $test2 = (string)$test2;
-    echo "test1: " . gettype($test1) . "<br />";
-    echo "test2: " . gettype($test2) . "<br />";
-?>
+            settype($test1, "string");
+            $test2 = (string)$test2;
+            echo "test1: " . gettype($test1) . "<br />";
+            echo "test2: " . gettype($test2) . "<br />";
+    ?>
 </body>
 </html>
