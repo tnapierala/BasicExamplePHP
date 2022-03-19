168d8e5ea32f2082d7269b839b4152e1aaab269e
tnapierala <tnapierala98@gmail.com>
2022-03-19 22:53:40 +0100

    CleningCode

--- test_019.php
+++ test_019.php
@@ -2,31 +2,28 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title> <?php echo $t ?> </title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <title> <?php echo $t ?> </title>
+    </head>
+    <body>
+        <h1> Lesson19 </h1>
 
-<body>
+        <?php
+            //function1
+            echo "<h4> Example1 </h4>";
+            function say_hello() {
+                echo "Hello World! <br />";
+            }
+            say_hello();
 
-<h1> Lesson19 </h1>
+            //function2
+            echo "<h4> Example2 </h4>";
+            function say_hello_two($world) {
+                echo "Hello $world! <br />";
+            }
+            say_hello_two("Swiat!");
 
-<?php
-    //function1
-    echo "<h4> Example1 </h4>";
-    function say_hello() {
-        echo "Hello World! <br />";
-    }
-    say_hello();
-
-    //function2
-    echo "<h4> Example2 </h4>";
-    function say_hello_two($world) {
-        echo "Hello $world! <br />";
-    }
-    say_hello_two("Swiat!");
-
-?>
-
-</body>
+        ?>
+    </body>
 </html>
